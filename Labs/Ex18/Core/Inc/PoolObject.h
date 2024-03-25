cffb5b100c594560c0b852d5146a0f390f7d9730
DiegoLHendrix <119289719+DiegoLHendrix@users.noreply.github.com>
2024-03-25 17:55:22 -0400

    Ex 17 and 18

---
+++ Labs/Ex18/Core/Inc/PoolObject.h
@@ -0,0 +1,20 @@
+/*
+ * PoolObject.h
+ *
+ *  Created on: Mar 20, 2024
+ *      Author: diego
+ */
+
+#ifndef INC_POOLOBJECT_H_
+#define INC_POOLOBJECT_H_
+
+void InitSemaphores(void);
+
+void SetGreenFlashRate(int x);
+int GetGreenFlashRate(void);
+
+void SetRedFlashRate(int x);
+int GetRedFlashRate(void);
+
+
+#endif /* INC_POOLOBJECT_H_ */
---
+++ Labs/Ex18/Core/Src/PoolObject.c
@@ -0,0 +1,52 @@
+/*
+ * PoolObject.c
+ *
+ *  Created on: Mar 20, 2024
+ *      Author: diego
+ */
+#include "FreeRTOS.h"
+#include "semphr.h"
+#include "PoolObject.h"
+
+/*Definition of data pool using a structure*/
+typedef struct
+{
+	int GreenTaskFlashRate;
+	int RedTaskFlashRate;
+} FlashingRate;
+
+/*Initialization of data pool variables and assigning default values*/
+FlashingRate FlashingDataPool = {10, 1};
+
+/*Declaration of FreeRTOS Semaphore*/
+SemaphoreHandle_t CriticalResourceSemaphore;
+
+void InitSemaphores(void)
+{
+	/*Create and Give (i.e. RELEASE) a Semaphore*/
+	CriticalResourceSemaphore = xSemaphoreCreateBinary();
+	xSemaphoreGive(CriticalResourceSemaphore);
+}
+
+void SetGreenFlashRate(int x)
+{
+	xSemaphoreTake(CriticalResourceSemaphore, portMAX_DELAY);
+	FlashingDataPool.GreenTaskFlashRate = x;
+}
+
+int GetGreenFlashRate(void)
+{
+	return FlashingDataPool.GreenTaskFlashRate;
+}
+
+void SetRedFlashRate(int x)
+{
+	xSemaphoreTake(CriticalResourceSemaphore, portMAX_DELAY);
+	FlashingDataPool.RedTaskFlashRate = x;
+}
+
+int GetRedFlashRate(void)
+{
+	return FlashingDataPool.RedTaskFlashRate;
+}
+
